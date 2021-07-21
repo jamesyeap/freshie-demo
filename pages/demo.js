9879df603cff5bc1191697847233fb5bf115436c
James Yeap <jamesyeap.ys@gmail.com>
2021-07-21 22:35:51 +0800

    update landing page

---
+++ pages/demo.js
@@ -0,0 +1,36 @@
+import Head from 'next/head'
+import styles from '../styles/Demo.module.css'
+
+export default function Demo() {
+	return (
+	<div className="h-auto w-auto align-center justify-center m-10">
+		<Head>
+			<title>Demo</title>
+			<meta name="description" content="Our demo!" />
+		</Head>
+
+		<div>
+				<form className="w-auto">
+				<div className="flex flex-wrap mb-6">
+					<div className="w-full px-3">
+					<label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">
+						Let us know your thoughts!
+					</label>
+					<input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" placeholder="Your feedback" />
+					<button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="button">
+						Submit
+					</button>
+					</div>
+				</div>
+				</form>
+				
+				<div className="w-auto align-center justify-center">
+					<iframe
+					src="https://appetize.io/embed/rb306vv90qhg54j7w6750c04rm?device=iphone12pro"
+					width="378px" height="800px" frameBorder="0" scrolling="no"></iframe>
+				</div>
+			</div>
+
+		
+	</div>)
+}
