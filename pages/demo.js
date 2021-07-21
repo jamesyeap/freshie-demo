8bc5b17f701a6b38d5a1ccc160923750ffacff1f
James Yeap <jamesyeap.ys@gmail.com>
2021-07-21 22:43:36 +0800

    initial commit

--- pages/demo.js
+++ pages/demo.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import styles from '../styles/Demo.module.css'
 
 export default function Demo() {
 	return (
--- pages/index.js
+++ pages/index.js
@@ -1,59 +1,68 @@
 import Head from 'next/head'
-import Link from 'next/link'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Freshie</title>
-        <meta name="description" content="A project for NUS Orbital" />
+        <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className="font-extrabold text-7xl">
-          Welcome to Freshie!
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        <h1 className="text-6xl font-bold">
+          Freshie by{' '}
+          <a className="text-blue-600">
+            Team FitBob
+          </a>
         </h1>
 
-        <p className="font-light text-2xl mt-3">
-          A project made for NUS Orbital 2021.
+        <p className="mt-3 text-2xl">
+          A project made for{' '}
+          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
+            NUS Orbital 2021
+          </code>
         </p>
 
-        <div className={styles.grid}>
-          <Link href="/demo">
-            <a className={styles.card}>
-              <h2 className="font-bold">Try It Out! &rarr;</h2>
-              <p>Let us know your thoughts!</p>
-            </a>
-          </Link>
+        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
+          <a
+            href="/demo"
+            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+          >
+            <h3 className="text-2xl font-bold">Try it out! &rarr;</h3>
+            <p className="mt-4 text-xl">
+              Let us know your thoughts!
+            </p>
+          </a>
 
-          <Link href="https://freshieorbital.gitbook.io/team-fitbob/">
-            <a className={styles.card}>
-              <h2 className="font-bold">Our Docs &rarr;</h2>
-              <p>Challenges we faced.</p>
-            </a>
-          </Link>
+          <a
+            href="https://freshieorbital.gitbook.io/team-fitbob/"
+            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+          >
+            <h3 className="text-2xl font-bold">Our Docs &rarr;</h3>
+            <p className="mt-4 text-xl">
+              Challenges we faced
+            </p>
+          </a>
 
-          <Link href="https://github.com/jamesyeap/freshie">
-            <a
-              className={styles.card}
-            >
-              <h2 className="font-bold">Our Code &rarr;</h2>
-              <p>Access our GitHub repo here!</p>
-            </a>
-          </Link>
+          <a
+            href="https://github.com/jamesyeap/freshie"
+            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+          >
+            <h3 className="text-2xl font-bold">Our Code &rarr;</h3>
+            <p className="mt-4 text-xl">
+              Access our GitHub repo here!
+            </p>
+          </a>
 
-          <Link href="/demo">
-            <a
-              className={styles.card}
-            >
-              <h2 className="font-bold">Team FitBob &rarr;</h2>
-              <p>
-                About Us.
-              </p>
-            </a>
-          </Link>
+          <a
+            href="/demo"
+            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+          >
+            <h3 className="text-2xl font-bold">Team FitBob &rarr;</h3>
+            <p className="mt-4 text-xl">
+              About Us.
+            </p>
+          </a>
         </div>
       </main>
     </div>
